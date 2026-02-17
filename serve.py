4f1f1cf1d39533df070bd655a96a6fd874f4314f
spk <sparvez.ipa@gmail.com>
2026-02-17 22:31:53 +0600

    update serve port

--- serve.py
+++ serve.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger('waitress')
     
-    port = 5000
+    port = 5100
     print(f"Starting Holiday Duty Manager on http://0.0.0.0:{port}")
     print("Press Ctrl+C to stop.")
     
